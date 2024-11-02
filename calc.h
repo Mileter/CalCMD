df0a6d617115266f1d7ddf37e8d24fe29a6b56af
Mileter <90982151+Mileter@users.noreply.github.com>
2024-11-01 20:30:21 -0700

    Get Ready for Rewrite
    Making a plan for what to rewrite...

--- calc.h
+++ calc.h
@@ -10,4 +10,84 @@
 #include <cmath>    // mathematical functions
 #include <stack>    // for parsing purposes
 
-// TODO: Reference additional headers your program requires here.
+#ifdef HAS_GRAPH_LIBS
+// sdl
+#elif defined(HAS_CURSES)
+// curses
+#endif
+
+#define version   0.9
+#define copyright 2024
+
+using namespace std;
+
+// String operations
+vector<string> splitString(
+	const string str,
+    const string delimiter
+);
+vector<string> sliceString(
+	const vector<string>& v, 
+    int x,
+    int legnth
+);
+
+// Shell
+string getCommand();
+void printAbout();
+void printFunctions(
+	vector<string> func = {"No functions have been defined."}
+);
+void printHelp(
+	vector<string> func
+);
+int formattingError(
+	string sender
+);
+/* 
+int main();
+*/
+
+// Calculation
+double computeExpression(
+	string expression, 
+	int inputFormat = 1
+);
+
+// Calculation helpers
+int precedence(
+	string op
+);
+double applyOperation(
+	string op,
+	double a,
+	double b = 0.0
+);
+double evalulatePostfix(
+	string expr
+);
+double infixToPostfix(
+	string expr
+);
+double prefixToPostfix(
+	string expr
+);
+
+// Function framework
+void addUserFunction(
+	string name,
+	string expr,
+	string testexpr=name+"()"
+);
+void removeFunction(
+	string name
+);
+bool resolveFunction(
+	string name,
+	int argc
+)
+double computeFunction(
+	string name,
+	int argc,
+	double ** argv
+);
