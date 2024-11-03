d48cc0f0b983832ac325bd01afec2b5a47457634
Mileter <90982151+Mileter@users.noreply.github.com>
2024-11-02 22:34:17 -0700

    Move reserved func finding to binds.cpp
    Creating binds.cpp now!

--- calc.cpp
+++ calc.cpp
@@ -21,22 +21,7 @@ using namespace std;
 map<string, tuple<int, vector<string>, string, int>> func;
                                   // (name, (argc, argv mappings, evaluation string, expression mode))
 map<string, string> funcComments; // (name, comments)
-vector<string> funcReserved       // function names that connect to cmath bindings
-  = {                             // TODO: implement all the bindingss into 
-		"add", "sub", "mul", "div", "mod",                      // standard operators
-		"pow", "sqrt", "cbrt", "hypot",                         // extended standard
-		"sin", "cos", "tan", "acos", "asin", "atan", "atan2",   // simple trig bindings
-		"cosh", "sinh", "tanh", "acosh", "asinh", "atanh",      // hyperbolic functions
-		"log", "log10", "log1p", "log2", "logb", "modf",        // log functions
-		"scalbln", "exp", "frexp", "ldexp", "exp2", "expm1",    // exp functions
-		"erf", "erfc",                                          // error functions
-		"tgamma", "lgamma",                                     // gamma functions
-		"ceil", "floor", "round", "trunc", "rint", "nearbyint", // rounding functions
-		"fmod", "remainder", "remquo",                          // extended modulus functions
-		"dim", "max", "min",                                    //  min, max, difference_type
-		"abs", "fabs", "fma",                                   // abs is actually fabs here
-		"pi", "nan"                                             // constants
-	};
+
 // Trigonometry calculation setting
 bool usingRadians;
 
@@ -54,7 +39,7 @@ bool addUserFunction(
 		recordError("Function framework", "Cannot add function; `" + name + "` already exists.");
 		throw ERR_ALREADY_EXIST;
 	}
-	else if (std::find(funcReserved.begin(), funcReserved.end(), name) != funcReserved.end())
+	else if (isInBindings(name))
 	{
 		recordError("Function framework", "Cannot add function; `" + name + "` has been reserved for a math binding.");
 		throw ERR_ALREADY_RESERVED;
@@ -95,53 +80,8 @@ long double applyOperation(
 {
 	try
 	{
-		switch(op)
-		{
-			case "add":
-			case "+":
-			if(argc > 2) throw ERR_ARGC_MORE_THAN_OP;
-			if(argc < 2) throw ERR_ARGC_LESS_THAN_OP;
-			return argv[0] + argv[1];
-			break;
-			
-			case "sub":
-			case "-":
-			if(argc > 2) throw ERR_ARGC_MORE_THAN_OP;
-			if(argc < 2) throw ERR_ARGC_LESS_THAN_OP;
-			return argv[0] - argv[1];
-			break;
-			
-			case "mul":
-			case "*":
-			if(argc > 2) throw ERR_ARGC_MORE_THAN_OP;
-			if(argc < 2) throw ERR_ARGC_LESS_THAN_OP;
-			return argv[0] * argv[1];
-			break;
-			
-			case "div":
-			case "/":
-			if(argc > 2) throw ERR_ARGC_MORE_THAN_OP;
-			if(argc < 2) throw ERR_ARGC_LESS_THAN_OP;
-			if(argv[1] == 0.0) throw ERR_DIV_BY_ZERO;
-			return argv[0] / argv[1];
-			break;
-			
-			case "fmod":
-			case "mod":
-			case "%":
-			if(argc > 2) throw ERR_ARGC_MORE_THAN_OP;
-			if(argc < 2) throw ERR_ARGC_LESS_THAN_OP;
-			if(stold(argv[1]) == 0.0) throw ERR_DIV_BY_ZERO;
-			return fmodl(argv[0], argv[1]);
-			break;
-			
-			// implement all the other functions and bindings
-		}
-	}
-	catch(invalid_argument const & e)
-	{
-		recordError("Operation computer", "Could not convert string to long double; input is not a long double.");
-		throw ERR_NESTED_ERROR;
+		if(isInBindings(op))
+			return applyBindings(op, argc, argv);
 	}
 	catch(out_of_range const & e)
 	{
